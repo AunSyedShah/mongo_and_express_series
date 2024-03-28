52e8ce71efb11c0725a87e38954377e5d4e64d1a
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2024-03-28 14:19:02 +0500

    28/3/2024 # 2

--- day_2_28_3_2024/index.js
+++ day_2_28_3_2024/index.js
@@ -1,25 +1,15 @@
 import express from 'express';
 
-const app = express(); // initialize express app
+const app = express();
 
-
-// create a route for the app
-
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome to Home</h1>');
+app.get('/', function handler(req, res) {
+    res.status(200).json(
+        {
+            message: 'Hello World'
+        }
+    )
 });
 
-app.get('/student/:std_id', (req, res) => {
-    const { std_id } = req.params;
-    res.send(`<h1>Hello ${std_id}</h1>`);
+app.listen(3000, function handler() {
+    console.log('Server is listening on port 3000');
 });
-
-app.get('/add/:num1/:num2', (req, res) => {
-    const { num1, num2 } = req.params;
-    const sum = parseInt(num1) + parseInt(num2);
-    res.send(`<h1>The sum is ${sum}</h1>`);
-}
-);
-
-// listen to the server
-app.listen(3000);
