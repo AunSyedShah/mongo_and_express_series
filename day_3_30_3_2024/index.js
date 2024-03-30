865bcdd689360d5b2b2a5eefba90fb14ed659a4b
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2024-03-30 14:11:50 +0500

    create, view, delete

--- day_3_30_3_2024/index.js
+++ day_3_30_3_2024/index.js
@@ -17,13 +17,42 @@ app.get("/", (req, res) => {
 );
 
 app.post("/", (req, res) => {
-    const {stud_name} = req.body;
+    const { stud_name } = req.body;
     students.push(stud_name);
-    res.status(200).json(
+    res.status(201).json(
         {
             "message": "Student added successfully"
         }
     );
+}
+);
+
+app.delete("/:stud_name", (req, res) => {
+    const { stud_name } = req.params;
+    // students = students.filter(
+    //     function handler(value) {
+    //         return value !== stud_name;
+    //     }
+    // );
+    // get the index of the student
+    const index = students.indexOf(stud_name);
+    // if the student exists
+    if (index > -1) {
+        // remove the student
+        students.splice(index, 1);
+        res.status(200).json(
+            {
+                "message": "Student deleted successfully"
+            }
+        );
+    }
+    else {
+        res.status(404).json(
+            {
+                "message": "Student not found"
+            }
+        );
+    }
 }
 );
 
